d2a9fc08429bb7529047b7efee460da7c8ef975f
余磊 <2272757910@qq.com>
2024-03-22 18:06:43 +0800

    feat: 项目部署

--- dist/assets/usePageMethod-C1WzBd98.js
+++
@@ -1,1 +0,0 @@
-import{a as n}from"./index-D9s0nCnu.js";function u(){const t=n(),a=n();return{contentRef:t,headerRef:a,handleSearch:e=>{var o;console.log("queryInfo:",e),(o=t.value)==null||o.getPageList(e,!0)},handleReset:()=>{var e;(e=t.value)==null||e.getPageList(),a.value&&(a.value.formData=a.value.originFormData)}}}export{u};
