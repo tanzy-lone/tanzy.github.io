8a13db520cfe37c3cb0499091f828a151b55ee5d
余磊 <2272757910@qq.com>
2024-04-03 21:49:08 +0800

    feat: 项目完善

--- vite.config.ts
+++ vite.config.ts
@@ -139,8 +139,8 @@ export default defineConfig(({ mode }: ConfigEnv): UserConfig => {
 						}
 
 						// return 'index'
-					},
-					format: 'esm'
+					}
+					// format: 'esm'
 					// chunkFileNames: 'static/js/[name]-[hash].js',
 					// entryFileNames: 'static/js/[name]-[hash].js',
 					// assetFileNames: 'static/[ext]/[name]-[hash].[ext]'
